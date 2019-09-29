09c49712d9700a2f5849382870c8f4a2e0a7384b
Adrian Gjerstad <adgjerstad@gmail.com>
2019-09-29 19:33:30 +0000

    Added background color

--- test/script.js
+++ test/script.js
@@ -1,6 +1,9 @@
 Solarize(function() {
+  this.setColor('test', 200, 0, 0);
+
   let hello = new this.Label('Hello, World!');
   let hello2 = new this.Label('This is a label');
   hello2.locate(0, 20);
   hello2.text('This is a label\nplus a bit!');
+  hello2.backgroundColor(this.PALLETE.test);
 });
